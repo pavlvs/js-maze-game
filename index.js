532d453b6b1a16467b6824c8422c68312d7a9bfa
pavlvs <pavlvsxavier@gmail.com>
2021-09-09 09:25:27 +0100

    project setup

---
+++ index.js
@@ -0,0 +1,45 @@
+// cf⇥ const arrow function assignment
+
+// ta⇥ ternary statement
+// fl⇥ for loop (ES6)
+// fo⇥ for of loop (ES6)
+// ife⇥ else statement
+// tc⇥ try/catch
+
+// ae⇥ addEventListener
+// gi⇥ getElementById
+// gt⇥ getElementsByTagName
+// qs⇥ querySelector
+// cel⇥ createElement
+// heac⇥ appendChild
+// hecla⇥ classList.add
+// hect⇥ classList.toggle
+// hega⇥ getAttribute
+// hesa⇥ setAttribute
+// hera⇥ removeAttribute
+
+// cs⇥ class (ES6)
+// csx⇥ extend a class (ES6)
+// m⇥ method (ES6 syntax)
+// get⇥ getter (ES6 syntax)
+// set⇥ setter (ES6 syntax)
+
+// fan⇥ anonymous function
+// fn⇥ named function
+// asf⇥ async function
+// aa⇥ async arrow function with
+// af⇥ arrow function (ES6)
+// f⇥ arrow function with body (ES6)
+// fr⇥ arrow function with return (ES6)
+
+// ra⇥ return new array
+// rp⇥ return Promise (ES6)
+// tf⇥ this
+
+// fe⇥ forEach loop
+// map⇥ map function
+
+// st⇥ setTimeout
+// si⇥ setInterval
+
+console.log('Ahoy!')
